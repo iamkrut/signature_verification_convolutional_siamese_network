34c03c01a48a3ed96922ed75a12263e5cdf234f8
KrutPatel2257 <krutpatel22577@gmail.com>
2019-11-26 00:18:48 -0800

    triplet model working

--- test_contrastive_loss.py
+++ test_contrastive_loss.py
@@ -15,7 +15,7 @@ from torch import optim
 import torch.nn.functional as F
 import re
 from collections import defaultdict
-from tripletnet import TripletNetModel
+from signet import SigNetModel
 import itertools as it
 
 
@@ -39,10 +39,9 @@ class Config:
     forged_dir = "./signatures/full_forg/"
     train_batch_size = 16
     val_batch_size = 8
-    train_number_epochs = 50
+    train_number_epochs = 20
     lr = 1e-4
-    file_prefix = "triplet_"
-
+    file_prefix = "contrastive_"
 
 def format_image_name(filename):
     ref = re.findall(r'\d+', filename)
@@ -58,6 +57,25 @@ def generate_permutations(list_1, list_2=None):
         for a, b in it.combinations(list_1, 2):
             permutations.append((a, b))
     return permutations
+
+
+def compute_accuracy_roc(predictions, labels, d=0.5):
+    nsame = np.sum(labels == 1)
+    ndiff = np.sum(labels == 0)
+
+    idx1 = predictions.ravel() <= d
+    idx2 = predictions.ravel() > d
+
+    tp = np.sum(labels[idx1] == 1)
+    fp = np.sum(labels[idx1] == 0)
+    tn = np.sum(labels[idx2] == 0)
+    fn = np.sum(labels[idx2] == 1)
+
+    print(tp, fp, nsame)
+    print(tn, fn, ndiff)
+    acc = (tp + tn) / labels.shape[0]
+
+    return acc
 
 
 class SiameseNetworkDataset(Dataset):
@@ -76,13 +94,8 @@ class SiameseNetworkDataset(Dataset):
 
         # generating_permutations
         for _id in self.org.keys():
-            random.seed(10)
             self.forg[_id] = random.sample(generate_permutations(self.org[_id], self.forg[_id]), 276)
             self.org[_id] = generate_permutations(self.org[_id])
-
-            for i, org_tuple in enumerate(self.org[_id]):
-                o1, o2 = org_tuple
-                self.org[_id][i] = (o1, o2, self.forg[_id][i][1])
 
         # randomly sample signer _id's for training
         random.seed(3)
@@ -93,100 +106,56 @@ class SiameseNetworkDataset(Dataset):
         # combine org and forg to form train or val data
         if self.is_train:
             for _id in train_ids:
-                self.data.extend([(obs[0], obs[1], obs[2]) for obs in self.org[_id]])
+                self.data.extend([(obs[0], obs[1], 1.0) for obs in self.org[_id]])
+                self.data.extend([(obs[0], obs[1], 0.0) for obs in self.forg[_id]])
         else:
             val_ids = list(set(self.org.keys()).difference(set(train_ids))) # getting the validation ids
             for _id in val_ids:
-                self.data.extend([(obs[0], obs[1], obs[2]) for obs in self.org[_id]])
+                self.data.extend([(obs[0], obs[1], 1.0) for obs in self.org[_id]])
+                self.data.extend([(obs[0], obs[1], 0.0) for obs in self.forg[_id]])
 
         self.org.clear()
         self.forg.clear()
 
     def __getitem__(self, index):
 
-        img_0, img_1, img_2 = self.data[index]
+        img_0, img_1, label = self.data[index]
 
         img_0 = Image.open(img_0)
         img_1 = Image.open(img_1)
-        img_2 = Image.open(img_2)
         img_0 = img_0.convert("L")
         img_1 = img_1.convert("L")
-        img_2 = img_2.convert("L")
 
         if self.should_invert:
             img_0 = PIL.ImageOps.invert(img_0)
             img_1 = PIL.ImageOps.invert(img_1)
-            img_2 = PIL.ImageOps.invert(img_2)
 
         if self.transform is not None:
             img_0 = self.transform(img_0)
             img_1 = self.transform(img_1)
-            img_2 = self.transform(img_2)
 
-        return img_0, img_1, img_2
+        return img_0, img_1, label
 
     def __len__(self):
         return len(self.data)
 
 
-class TripletLoss(nn.Module):
+class ContrastiveLoss(torch.nn.Module):
     """
-    Triplet loss
-    Takes embeddings of an anchor sample, a positive sample and a negative sample
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
     """
 
-    def __init__(self, margin=1):
-        super(TripletLoss, self).__init__()
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
         self.margin = margin
-        self.accuracy = []
-        self.margin_loss = torch.nn.MarginRankingLoss(margin=self.margin)
 
-    def forward(self, distance_positive, distance_negative, target, size_average=True, is_val=False):
-        losses = self.margin_loss(distance_positive, distance_negative, target)
-        # max(0,−y∗(x1−x2) + margin)
-        # losses = F.relu(-1 * (distance_positive - distance_negative) + self.margin)
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2, keepdim = True)
+        loss_contrastive = torch.mean((label) * torch.pow(euclidean_distance, 2) +
+                                      ((1-label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)))
 
-        if is_val:
-            # pred = (-1 * (distance_positive - distance_negative) + self.margin).data
-            # print(losses)
-            # print((losses == 0).sum(), "/", distance_positive.size()[0])
-            margin = 0
-            pred = (distance_positive - distance_negative - margin).cpu().data
-            self.accuracy.append(((pred > 0).sum() * 1.0 / distance_positive.size()[0]).item())
-
-        return losses.mean() if size_average else losses.sum()
-
-
-class OnlineTripletLoss(nn.Module):
-    """
-    Online Triplets loss
-    Takes a batch of embeddings and corresponding labels.
-    Triplets are generated using triplet_selector object that take embeddings and targets and return indices of
-    triplets
-    """
-
-    def __init__(self, margin, triplet_selector):
-        super(OnlineTripletLoss, self).__init__()
-        self.margin = margin
-        self.triplet_selector = triplet_selector
-        self.accuracy = []
-
-    def forward(self, embeddings, target, is_val=False):
-
-        triplets = self.triplet_selector.get_triplets(embeddings, target)
-
-        if embeddings.is_cuda:
-            triplets = triplets.cuda()
-
-        ap_distances = (embeddings[triplets[:, 0]] - embeddings[triplets[:, 1]]).pow(2).sum(1)  # .pow(.5)
-        an_distances = (embeddings[triplets[:, 0]] - embeddings[triplets[:, 2]]).pow(2).sum(1)  # .pow(.5)
-        losses = F.relu(ap_distances - an_distances + self.margin)
-
-        if is_val:
-            pred = (ap_distances - an_distances - self.margin).data
-            self.accuracy.append(((pred > 0).sum()*1.0/distance_positive.size()[0]).item())
-
-        return losses.mean(), len(triplets)
+        return loss_contrastive
 
 
 if __name__ == '__main__':
@@ -212,18 +181,18 @@ if __name__ == '__main__':
     vis_dataloader = DataLoader(train_dataset, shuffle=True, num_workers=8, batch_size=8)
     dataiter = iter(vis_dataloader)
 
-    example_batch = next(dataiter)
-    concatenated = torch.cat((example_batch[0],example_batch[1]),0)
-    # torchvision.utils.save_image((concatenated), 'train_data.png')
+    # example_batch = next(dataiter)
+    # concatenated = torch.cat((example_batch[0],example_batch[1]),0)
+    # # torchvision.utils.save_image((concatenated), 'train_data.png')
     # imshow(torchvision.utils.make_grid(concatenated))
     # print(example_batch[2].numpy())
 
     train_dataloader = DataLoader(train_dataset, shuffle=True, num_workers=8, batch_size=Config.train_batch_size)
     val_dataloader = DataLoader(val_dataset, shuffle=True, num_workers=2, batch_size=Config.val_batch_size)
 
-    net = TripletNetModel()
+    net = SigNetModel()
 
-    criterion = TripletLoss()
+    criterion = ContrastiveLoss()
     # optimizer = optim.RMSprop(net.parameters(), lr=Config.lr, eps=1e-8, weight_decay=0.0005, momentum=0.9)
     optimizer = optim.Adam(net.parameters(), lr=Config.lr, weight_decay=0.0001)
 
@@ -238,53 +207,56 @@ if __name__ == '__main__':
     past_acc = 0.0
     for epoch in range(0, Config.train_number_epochs):
 
+        # adjusting learning rate
+        # lr = Config.lr * (0.1 ** epoch)
+        # for param_group in optimizer.param_groups:
+        #     param_group['lr'] = lr
+        #
+        # print("lr: ", lr)
+
         acc_train_loss = 0
         acc_val_loss = 0
+        avg_acc = 0
+        avg_distance_threshold = 0
+        val_predictions = []
+        val_labels = []
 
         # training
         net.train()
-        criterion.accuracy = []
         for i, data in enumerate(train_dataloader, 0):
-            img0, img1, img2 = data
+            img0, img1, label = data
             if is_cuda:
-                img0, img1 , img2 = img0.cuda(), img1.cuda(), img2.cuda()
+                img0, img1 , label = img0.cuda(), img1.cuda(), label.cuda()
             optimizer.zero_grad()
-            output1, output2, output3 = net(img0, img1, img2)
-            distance_positive = F.pairwise_distance(output1, output2, keepdim = True)
-            distance_negative = F.pairwise_distance(output1, output3, keepdim = True)
-            target = torch.FloatTensor(distance_positive.size()).fill_(1)
-            if is_cuda:
-                target = target.cuda()
-            loss_triplet = criterion(distance_positive, distance_negative, target, is_val=True)
-            loss_triplet.backward()
+            output1, output2 = net(img0, img1)
+            loss_contrastive = criterion(output1, output2, label)
+            loss_contrastive.backward()
             optimizer.step()
-            acc_train_loss += loss_triplet.item()
+            acc_train_loss += loss_contrastive.item()
         metric_history['train'].append(acc_train_loss / (i+1))
-        print("Train acc ", sum(criterion.accuracy) / len(criterion.accuracy))
 
         # validation
         net.eval()
-        criterion.accuracy = []
         for i, data in enumerate(val_dataloader, 0):
-            img0, img1, img2 = data
+            img0, img1, label = data
             if is_cuda:
-                img0, img1 , img2 = img0.cuda(), img1.cuda(), img2.cuda()
-            output1, output2, output3 = net(img0, img1, img2)
+                img0, img1 , label = img0.cuda(), img1.cuda(), label.cuda()
+            output1, output2 = net(img0, img1)
+            loss_contrastive = criterion(output1, output2, label)
+            acc_val_loss += loss_contrastive.item()
 
-            distance_positive = F.pairwise_distance(output1, output2, keepdim=True)
-            distance_negative = F.pairwise_distance(output1, output3, keepdim=True)
-            target = torch.FloatTensor(distance_positive.size()).fill_(1)
+            # predictions
+            euclidean_distance = F.pairwise_distance(output1, output2, keepdim=True)
             if is_cuda:
-                target = target.cuda()
-            loss_triplet = criterion(distance_positive, distance_negative, target, is_val=True)
-            acc_val_loss += loss_triplet.item()
+                euclidean_distance = euclidean_distance.cpu()
+                label = label.cpu()
+            val_predictions.extend(euclidean_distance.detach().numpy())
+            val_labels.extend(label.detach().numpy())
 
+            # concatenated = torch.cat((img0, img1), 0)
+            # torchvision.utils.save_image((concatenated), 'Dissimilarity: {:.2f}'.format(euclidean_distance.item()), 'val_'+epoch+'.png')
 
-        print(criterion.accuracy)
-        print(sum(criterion.accuracy))
-        print(len(criterion.accuracy))
-
-        acc = sum(criterion.accuracy) / len(criterion.accuracy)
+        acc = compute_accuracy_roc(np.array(val_predictions), np.array(val_labels))
         if acc > past_acc:
             past_acc = acc
             torch.save(net.state_dict(), Config.file_prefix + 'cedar_cl.dth')
@@ -292,8 +264,8 @@ if __name__ == '__main__':
 
         metric_history['val'].append(acc_val_loss / (i+1))
         metric_history['acc'].append(acc)
-        print("\nEpoch number {} Train loss {} Val loss {}".format(epoch, metric_history['train'][-1], metric_history['val'][-1]))
-        print('Accuracy {}'.format(acc))
+        print("Epoch number {} Train loss {} Val loss {}".format(epoch, metric_history['train'][-1], metric_history['val'][-1]))
+        print('Val accuracy {}\n'.format(acc))
 
     # plot loss metric
     plt.figure()
--- tripletnet.py
+++ tripletnet.py
@@ -1,59 +1,80 @@
 from torch import nn
 
 class TripletNetModel(nn.Module):
-    """ SigNet model, from https://arxiv.org/abs/1705.05787
-    """
+
+    # def __init__(self):
+    #     super(TripletNetModel, self).__init__()
+    #
+    #     self.conv1 = nn.Sequential(
+    #         nn.Conv2d(in_channels=1, out_channels=96, kernel_size=11, stride=1),
+    #         # nn.LocalResponseNorm(size=96, alpha=0.0001, beta=0.75, k=2),
+    #         nn.BatchNorm2d(96, eps=1e-06, momentum=0.9),
+    #         nn.ReLU(),
+    #         nn.MaxPool2d(kernel_size=3, stride=2)
+    #     )
+    #     self.conv2 = nn.Sequential(
+    #         nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2),
+    #         # nn.LocalResponseNorm(size=256, alpha=0.0001, beta=0.75, k=2),
+    #         nn.BatchNorm2d(256, eps=1e-06, momentum=0.9),
+    #         nn.ReLU(),
+    #         nn.MaxPool2d(kernel_size=3, stride=2),
+    #         nn.Dropout2d(p=0.3),
+    #     )
+    #     self.conv3 = nn.Sequential(
+    #         nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1),
+    #         nn.BatchNorm2d(384, eps=1e-06, momentum=0.9),
+    #         nn.ReLU()
+    #     )
+    #     self.conv4 = nn.Sequential(
+    #         nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1),
+    #         nn.BatchNorm2d(256, eps=1e-06, momentum=0.9),
+    #         nn.ReLU(),
+    #         nn.MaxPool2d(kernel_size=3, stride=2),
+    #         nn.Dropout2d(p=0.3)
+    #     )
+    #     self.linear1 = nn.Sequential(
+    #         nn.Linear(256 * 17 * 25, 1024),
+    #         nn.BatchNorm1d(1024),
+    #         nn.ReLU(),
+    #         nn.Dropout2d(p=0.5)
+    #     )
+    #     self.linear2 = nn.Sequential(
+    #         nn.Linear(1024, 128),
+    #         nn.BatchNorm1d(128),
+    #         nn.ReLU()
+    #     )
+    #
+    # def forward_once(self, x):
+    #     x = self.conv1(x)
+    #     x = self.conv2(x)
+    #     x = self.conv3(x)
+    #     x = self.conv4(x)
+    #     x = x.view(x.shape[0], 256 * 17 * 25)
+    #     x = self.linear1(x)
+    #     x = self.linear2(x)
+    #     return x
+    #
+
+
     def __init__(self):
         super(TripletNetModel, self).__init__()
+        self.convnet = nn.Sequential(nn.Conv2d(1, 32, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2),
+                                     nn.Conv2d(32, 64, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2))
 
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=96, kernel_size=11, stride=1),
-            # nn.LocalResponseNorm(size=96, alpha=0.0001, beta=0.75, k=2),
-            nn.BatchNorm2d(96, eps=1e-06, momentum=0.9),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2)
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2),
-            # nn.LocalResponseNorm(size=256, alpha=0.0001, beta=0.75, k=2),
-            nn.BatchNorm2d(256, eps=1e-06, momentum=0.9),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Dropout2d(p=0.3),
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(384, eps=1e-06, momentum=0.9),
-            nn.ReLU()
-        )
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256, eps=1e-06, momentum=0.9),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Dropout2d(p=0.3)
-        )
-        self.linear1 = nn.Sequential(
-            nn.Linear(256 * 17 * 25, 1024),
-            nn.BatchNorm1d(1024),
-            nn.ReLU(),
-            nn.Dropout2d(p=0.5)
-        )
-        self.linear2 = nn.Sequential(
-            nn.Linear(1024, 128),
-            nn.BatchNorm1d(128),
-            nn.ReLU()
-        )
+        self.fc = nn.Sequential(nn.Linear(64 * 35 * 52, 2048),
+                                nn.PReLU(),
+                                nn.Linear(2048, 1024),
+                                nn.PReLU(),
+                                nn.Linear(1024, 128)
+                                )
 
     def forward_once(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = x.view(x.shape[0], 256 * 17 * 25)
-        x = self.linear1(x)
-        x = self.linear2(x)
-        return x
+        output = self.convnet(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc(output)
+        return output
 
     def forward(self, input1, input2, input3):
         output1 = self.forward_once(input1)
