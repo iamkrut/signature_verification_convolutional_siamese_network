60b6506a27adebfc9ca0bcb92d4e5413e712d46a
KrutPatel2257 <krutpatel22577@gmail.com>
2019-11-26 23:32:16 -0800

    triplet loss testing on BHSig260

--- main_triplet_loss.py
+++ main_triplet_loss.py
@@ -39,6 +39,7 @@ class Config:
     forged_dir = "./signatures/full_forg/"
     train_batch_size = 16
     val_batch_size = 8
+    test_batch_size = 16
     train_number_epochs = 50
     lr = 1e-4
     file_prefix = "triplet_"
@@ -129,6 +130,62 @@ class SiameseNetworkDataset(Dataset):
         return len(self.data)
 
 
+class BHSig260Dataset(Dataset):
+
+    def __init__(self, folder, dataset_pair_file, transform=None, should_invert=True):
+        self.folder = folder
+        self.dataset_pair_file = self.folder + dataset_pair_file
+        self.transform = transform
+        self.should_invert = should_invert
+        self.org = defaultdict(list)
+        self.forg = defaultdict(list)
+        self.data = []
+
+        with open(self.dataset_pair_file) as fp:
+            for _, line in enumerate(fp):
+                pair = line.strip().split(" ")
+                _id = int(pair[0][:3])
+                label = int(pair[2])
+                if label == 1:
+                    self.org[_id].append((pair[0], pair[1]))
+                else:
+                    self.forg[_id].append((pair[0], pair[1]))
+
+        for _id in self.org.keys():
+            for i, org_tuple in enumerate(self.org[_id]):
+                o1, o2 = org_tuple
+                self.org[_id][i] = (o1, o2, self.forg[_id][i][1])
+
+        for _id in self.org.keys():
+            self.data.extend([(obs[0], obs[1], obs[2]) for obs in self.org[_id]])
+
+    def __getitem__(self, index):
+
+        img_0, img_1, img_2 = self.data[index]
+
+        img_0 = Image.open(self.folder + img_0)
+        img_1 = Image.open(self.folder + img_1)
+        img_2 = Image.open(self.folder + img_2)
+        img_0 = img_0.convert("L")
+        img_1 = img_1.convert("L")
+        img_2 = img_2.convert("L")
+
+        if self.should_invert:
+            img_0 = PIL.ImageOps.invert(img_0)
+            img_1 = PIL.ImageOps.invert(img_1)
+            img_2 = PIL.ImageOps.invert(img_2)
+
+        if self.transform is not None:
+            img_0 = self.transform(img_0)
+            img_1 = self.transform(img_1)
+            img_2 = self.transform(img_2)
+
+        return img_0, img_1, img_2
+
+    def __len__(self):
+        return len(self.data)
+
+
 class TripletLoss(nn.Module):
     """
     Triplet loss
@@ -157,118 +214,170 @@ class TripletLoss(nn.Module):
         return losses.mean() if size_average else losses.sum()
 
 
-class OnlineTripletLoss(nn.Module):
-    """
-    Online Triplets loss
-    Takes a batch of embeddings and corresponding labels.
-    Triplets are generated using triplet_selector object that take embeddings and targets and return indices of
-    triplets
-    """
-
-    def __init__(self, margin, triplet_selector):
-        super(OnlineTripletLoss, self).__init__()
-        self.margin = margin
-        self.triplet_selector = triplet_selector
-        self.accuracy = []
-
-    def forward(self, embeddings, target, is_val=False):
-
-        triplets = self.triplet_selector.get_triplets(embeddings, target)
-
-        if embeddings.is_cuda:
-            triplets = triplets.cuda()
-
-        ap_distances = (embeddings[triplets[:, 0]] - embeddings[triplets[:, 1]]).pow(2).sum(1)  # .pow(.5)
-        an_distances = (embeddings[triplets[:, 0]] - embeddings[triplets[:, 2]]).pow(2).sum(1)  # .pow(.5)
-        losses = F.relu(ap_distances - an_distances + self.margin)
-
-        if is_val:
-            pred = (ap_distances - an_distances - self.margin).data
-            self.accuracy.append(((pred > 0).sum()*1.0/distance_positive.size()[0]).item())
-
-        return losses.mean(), len(triplets)
-
-
 if __name__ == '__main__':
+    train = False
 
     is_cuda = torch.cuda.is_available()
     print("Is cuda availiable: ", is_cuda)
 
-    folder_dataset = dset.ImageFolder(root=Config.original_dir)
-    train_dataset = SiameseNetworkDataset(image_folder_dataset=folder_dataset,
-                                          transform=transforms.Compose([transforms.Resize((155, 220), interpolation=PIL.Image.BILINEAR),
-                                                                        transforms.ToTensor(),
-                                                                        transforms.Normalize(mean=[0.5], std=[0.5])]),
-                                          should_invert=True,
-                                          is_train=True)
-    val_dataset = SiameseNetworkDataset(image_folder_dataset=folder_dataset,
-                                        transform=transforms.Compose([transforms.Resize((155, 220), interpolation=PIL.Image.BILINEAR),
-                                                                      transforms.ToTensor(),
-                                                                      transforms.Normalize(mean=[0.5], std=[0.5])]),
-                                        should_invert=True,
-                                        is_train=False)
+    if train:
+        folder_dataset = dset.ImageFolder(root=Config.original_dir)
+        train_dataset = SiameseNetworkDataset(image_folder_dataset=folder_dataset,
+                                              transform=transforms.Compose([transforms.Resize((155, 220), interpolation=PIL.Image.BILINEAR),
+                                                                            transforms.ToTensor(),
+                                                                            transforms.Normalize(mean=[0.5], std=[0.5])]),
+                                              should_invert=True,
+                                              is_train=True)
+        val_dataset = SiameseNetworkDataset(image_folder_dataset=folder_dataset,
+                                            transform=transforms.Compose([transforms.Resize((155, 220), interpolation=PIL.Image.BILINEAR),
+                                                                          transforms.ToTensor(),
+                                                                          transforms.Normalize(mean=[0.5], std=[0.5])]),
+                                            should_invert=True,
+                                            is_train=False)
 
-    # visual some sample data
-    vis_dataloader = DataLoader(train_dataset, shuffle=True, num_workers=8, batch_size=8)
-    dataiter = iter(vis_dataloader)
+        # visual some sample data
+        vis_dataloader = DataLoader(train_dataset, shuffle=True, num_workers=8, batch_size=8)
+        dataiter = iter(vis_dataloader)
 
-    example_batch = next(dataiter)
-    concatenated = torch.cat((example_batch[0],example_batch[1]),0)
-    # torchvision.utils.save_image((concatenated), 'train_data.png')
-    # imshow(torchvision.utils.make_grid(concatenated))
-    # print(example_batch[2].numpy())
+        example_batch = next(dataiter)
+        concatenated = torch.cat((example_batch[0],example_batch[1]),0)
+        # torchvision.utils.save_image((concatenated), 'train_data.png')
+        # imshow(torchvision.utils.make_grid(concatenated))
+        # print(example_batch[2].numpy())
 
-    train_dataloader = DataLoader(train_dataset, shuffle=True, num_workers=8, batch_size=Config.train_batch_size)
-    val_dataloader = DataLoader(val_dataset, shuffle=True, num_workers=2, batch_size=Config.val_batch_size)
+        train_dataloader = DataLoader(train_dataset, shuffle=True, num_workers=8, batch_size=Config.train_batch_size)
+        val_dataloader = DataLoader(val_dataset, shuffle=True, num_workers=2, batch_size=Config.val_batch_size)
 
-    net = TripletNetModel()
+        net = TripletNetModel()
 
-    criterion = TripletLoss()
-    # optimizer = optim.RMSprop(net.parameters(), lr=Config.lr, eps=1e-8, weight_decay=0.0005, momentum=0.9)
-    optimizer = optim.Adam(net.parameters(), lr=Config.lr, weight_decay=0.0001)
+        criterion = TripletLoss()
+        # optimizer = optim.RMSprop(net.parameters(), lr=Config.lr, eps=1e-8, weight_decay=0.0005, momentum=0.9)
+        optimizer = optim.Adam(net.parameters(), lr=Config.lr, weight_decay=0.0001)
 
-    if is_cuda:
-        net.cuda()
+        if is_cuda:
+            net.cuda()
 
-    metric_history = defaultdict(list)
+        metric_history = defaultdict(list)
 
-    print(net)
-    print("Train data: ", len(train_dataset))
-    print("Val data: ", len(val_dataset))
-    past_acc = 0.0
-    for epoch in range(0, Config.train_number_epochs):
+        print(net)
+        print("Train data: ", len(train_dataset))
+        print("Val data: ", len(val_dataset))
+        past_acc = 0.0
+        for epoch in range(0, Config.train_number_epochs):
 
-        acc_train_loss = 0
-        acc_val_loss = 0
+            acc_train_loss = 0
+            acc_val_loss = 0
 
-        # training
-        net.train()
-        criterion.accuracy = []
-        for i, data in enumerate(train_dataloader, 0):
-            img0, img1, img2 = data
-            if is_cuda:
-                img0, img1 , img2 = img0.cuda(), img1.cuda(), img2.cuda()
-            optimizer.zero_grad()
-            output1, output2, output3 = net(img0, img1, img2)
-            distance_positive = F.pairwise_distance(output1, output2, keepdim = True)
-            distance_negative = F.pairwise_distance(output1, output3, keepdim = True)
-            target = torch.FloatTensor(distance_positive.size()).fill_(1)
-            if is_cuda:
-                target = target.cuda()
-            loss_triplet = criterion(distance_positive, distance_negative, target, is_val=True)
-            loss_triplet.backward()
-            optimizer.step()
-            acc_train_loss += loss_triplet.item()
-        metric_history['train'].append(acc_train_loss / (i+1))
-        print("Train acc ", sum(criterion.accuracy) / len(criterion.accuracy))
+            # training
+            net.train()
+            criterion.accuracy = []
+            for i, data in enumerate(train_dataloader, 0):
+                img0, img1, img2 = data
+                if is_cuda:
+                    img0, img1 , img2 = img0.cuda(), img1.cuda(), img2.cuda()
+                optimizer.zero_grad()
+                output1, output2, output3 = net(img0, img1, img2)
+                distance_positive = F.pairwise_distance(output1, output2, keepdim = True)
+                distance_negative = F.pairwise_distance(output1, output3, keepdim = True)
+                target = torch.FloatTensor(distance_positive.size()).fill_(1)
+                if is_cuda:
+                    target = target.cuda()
+                loss_triplet = criterion(distance_positive, distance_negative, target, is_val=True)
+                loss_triplet.backward()
+                optimizer.step()
+                acc_train_loss += loss_triplet.item()
+            metric_history['train'].append(acc_train_loss / (i+1))
+            print("Train acc ", sum(criterion.accuracy) / len(criterion.accuracy))
 
-        # validation
+            # validation
+            net.eval()
+            criterion.accuracy = []
+            for i, data in enumerate(val_dataloader, 0):
+                img0, img1, img2 = data
+                if is_cuda:
+                    img0, img1 , img2 = img0.cuda(), img1.cuda(), img2.cuda()
+                output1, output2, output3 = net(img0, img1, img2)
+
+                distance_positive = F.pairwise_distance(output1, output2, keepdim=True)
+                distance_negative = F.pairwise_distance(output1, output3, keepdim=True)
+                target = torch.FloatTensor(distance_positive.size()).fill_(1)
+                if is_cuda:
+                    target = target.cuda()
+                loss_triplet = criterion(distance_positive, distance_negative, target, is_val=True)
+                acc_val_loss += loss_triplet.item()
+
+
+            print(criterion.accuracy)
+            print(sum(criterion.accuracy))
+            print(len(criterion.accuracy))
+
+            acc = sum(criterion.accuracy) / len(criterion.accuracy)
+            if acc > past_acc:
+                past_acc = acc
+                torch.save(net.state_dict(), Config.file_prefix + 'cedar_cl.dth')
+                print("Saving the model parameters")
+
+            metric_history['val'].append(acc_val_loss / (i+1))
+            metric_history['acc'].append(acc)
+            print("\nEpoch number {} Train loss {} Val loss {}".format(epoch, metric_history['train'][-1], metric_history['val'][-1]))
+            print('Accuracy {}'.format(acc))
+
+        # plot loss metric
+        plt.figure()
+        plt.clf()
+        iter = [i for i in range(0, epoch+1)]
+        plt.plot(iter, metric_history['train'], '-r', label='train loss')
+        plt.plot(iter, metric_history['val'], '-b', label='val loss')
+        # plt.plot(iter, metric_history['acc'], '-g', label='val accuracy')
+        plt.xlabel("epochs")
+        plt.ylabel("loss")
+        plt.legend(loc='upper left')
+        plt.savefig(Config.file_prefix+'loss_plot.png')
+
+        # plot accuracy
+        plt.figure()
+        iter = [i for i in range(0, epoch + 1)]
+        plt.plot(iter, metric_history['acc'], '-g', label='accuracy')
+        plt.xlabel("epochs")
+        plt.ylabel("accuracy")
+        plt.legend(loc='upper left')
+        plt.savefig(Config.file_prefix + 'acc_plot.png')
+
+    else:
+        print("Testing on BHSig260 Bengali dataset")
+        # Testing on BHSig260
+        test_dataset = BHSig260Dataset(folder="BHSig260/Bengali/", dataset_pair_file="Bengali_pairs.txt", transform=transforms.Compose(
+                                                  [transforms.Resize((155, 220), interpolation=PIL.Image.BILINEAR),
+                                                   transforms.ToTensor(),
+                                                   transforms.Normalize(mean=[0.5], std=[0.5])]),
+                                        should_invert=True)
+
+        test_dataloader = DataLoader(test_dataset, shuffle=True, num_workers=8, batch_size=Config.test_batch_size)
+
+        net = TripletNetModel()
+
+        # loading net state dict
+        if is_cuda:
+            net.load_state_dict(torch.load("triplet_cedar_cl.dth"))
+        else:
+            net.load_state_dict(torch.load("triplet_cedar_cl.dth", map_location=torch.device('cpu')))
+
+        if is_cuda:
+            net.cuda()
+
+        print("Test data: ", len(test_dataset))
+
+        test_predictions = []
+        test_labels = []
+
+        criterion = TripletLoss()
+
+        # testing
         net.eval()
-        criterion.accuracy = []
-        for i, data in enumerate(val_dataloader, 0):
+        for i, data in enumerate(test_dataloader, 0):
             img0, img1, img2 = data
             if is_cuda:
-                img0, img1 , img2 = img0.cuda(), img1.cuda(), img2.cuda()
+                img0, img1, img2 = img0.cuda(), img1.cuda(), img2.cuda()
             output1, output2, output3 = net(img0, img1, img2)
 
             distance_positive = F.pairwise_distance(output1, output2, keepdim=True)
@@ -277,41 +386,6 @@ if __name__ == '__main__':
             if is_cuda:
                 target = target.cuda()
             loss_triplet = criterion(distance_positive, distance_negative, target, is_val=True)
-            acc_val_loss += loss_triplet.item()
-
-
-        print(criterion.accuracy)
-        print(sum(criterion.accuracy))
-        print(len(criterion.accuracy))
 
         acc = sum(criterion.accuracy) / len(criterion.accuracy)
-        if acc > past_acc:
-            past_acc = acc
-            torch.save(net.state_dict(), Config.file_prefix + 'cedar_cl.dth')
-            print("Saving the model parameters")
-
-        metric_history['val'].append(acc_val_loss / (i+1))
-        metric_history['acc'].append(acc)
-        print("\nEpoch number {} Train loss {} Val loss {}".format(epoch, metric_history['train'][-1], metric_history['val'][-1]))
-        print('Accuracy {}'.format(acc))
-
-    # plot loss metric
-    plt.figure()
-    plt.clf()
-    iter = [i for i in range(0, epoch+1)]
-    plt.plot(iter, metric_history['train'], '-r', label='train loss')
-    plt.plot(iter, metric_history['val'], '-b', label='val loss')
-    # plt.plot(iter, metric_history['acc'], '-g', label='val accuracy')
-    plt.xlabel("epochs")
-    plt.ylabel("loss")
-    plt.legend(loc='upper left')
-    plt.savefig(Config.file_prefix+'loss_plot.png')
-
-    # plot accuracy
-    plt.figure()
-    iter = [i for i in range(0, epoch + 1)]
-    plt.plot(iter, metric_history['acc'], '-g', label='accuracy')
-    plt.xlabel("epochs")
-    plt.ylabel("accuracy")
-    plt.legend(loc='upper left')
-    plt.savefig(Config.file_prefix + 'acc_plot.png')
+        print('Test Accuracy {}'.format(acc))
